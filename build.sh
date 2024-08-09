9f41aa475a326f64a58545f981fa3618e1f68851
Joshua AE Lee <josh@10leej.com>
2024-08-09 15:03:20 -0400

    add some desktop stuff

--- build.sh
+++ build.sh
@@ -13,10 +13,10 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install screen
+rpm-ostree install sway swayidle swaylock grim slurp wl-clipboard
 
 # this would install a package from rpmfusion
-# rpm-ostree install vlc
+# rpm-ostree install mpv
 
 #### Example for enabling a System Unit File
 
