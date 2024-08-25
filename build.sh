f924a6f49822bde1a1989273e476002d6e78c751
Joshua AE Lee <josh@10leej.com>
2024-08-25 12:48:28 -0400

    hey steve wine works check #28

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install sway swayidle swaylock grim slurp wl-clipboard vim htop iftop iotop tmux git firefox greetd greetd-selinux tuigreet emacs libtool pdf-tools cmake wine
+rpm-ostree install sway swayidle swaylock grim slurp wl-clipboard vim htop iftop iotop tmux git firefox greetd greetd-selinux tuigreet emacs libtool pdf-tools cmake
 
 # this would install a package from rpmfusion
 rpm-ostree install mpv yt-dlp
